5dbad83271b5a11adcb239302fef330d24a1d37b
Gleb <gmarshell21@gmail.com>
2024-01-15 21:55:42 -0500

    Week2 added

---
+++ WEEK2/script.js
@@ -0,0 +1,19 @@
+// Task 1 Use JavaScript to put the red border around our paragraph
+
+// Select the <p> element and store it inside a variable
+
+// Execute a method to modify that <p> element and change its border to red
+
+let car = {
+    color: "red",
+    make: "honda",
+    year: 2015
+}
+
+
+
+const paragraphElement = document.querySelector('p')
+
+console.log(paragraphElement);
+
+paragraphElement.style.border = "2px solid red"
